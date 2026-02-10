ffa92c5fe12aa0f3ab7510b6df4f9e69f4136cad
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2026-02-10 23:49:07 +0100

    Added tests and fixed bugs

--- src/vchat/packet.rs
+++ src/vchat/packet.rs
@@ -22,7 +22,7 @@ pub struct Packet {
 /// 2 * 10^2 = 200
 /// 200 + 50 = 250
 /// Version is 250
-#[derive(Debug, Clone, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Header {
     version: u32,
     timestamp: chrono::DateTime<chrono::Utc>,
@@ -57,7 +57,7 @@ impl Packet {
     pub fn to_bytes(self) -> Vec<u8> {
         let mut buf = self.payload;
         let len = buf.len();
-        buf.reserve_exact(HEADER_LEN);
+        buf.resize(HEADER_LEN + buf.len(), 0);
         buf.copy_within(..len, HEADER_LEN);
 
         self.header.to_bytes(&mut buf[..HEADER_LEN]);
@@ -76,7 +76,7 @@ impl From<Vec<u8>> for Packet {
 impl Header {
     pub fn new(payload: &[u8]) -> Self {
         let len = payload.len();
-        if len < u16::MAX as usize {
+        if len > u16::MAX as usize {
             panic!(
                 "Payload too big: Payload must be less or equal to {}",
                 u16::MAX
@@ -146,5 +146,82 @@ impl Header {
             timestamp,
             checksum,
         }
+    }
+}
+
+#[cfg(test)]
+mod packet_test {
+    use crate::vchat::packet::{HEADER_LEN, Header, Packet};
+
+    const PAYLOAD: [u8; 6] = [25, 40, 90, 120, 30, 0];
+
+    #[test]
+    fn header_to_bytes() {
+        let header = get_header();
+
+        let control_bytes = get_header_control_bytes(&header);
+
+        let mut header_bytes = [0u8; HEADER_LEN];
+        header.to_bytes(&mut header_bytes);
+
+        assert_eq!(header_bytes, control_bytes);
+    }
+
+    #[test]
+    fn header_from_bytes() {
+        let mut control_header = get_header();
+        let mut header_bytes = [0u8; HEADER_LEN];
+        control_header.to_bytes(&mut header_bytes);
+
+        let header = Header::from_bytes(header_bytes);
+        // `Header::from_bytes` uses milliseconds but the original uses the full capabilities of the Computer.
+        // They won't match most of the time so set the control to the less exact version.
+        control_header.timestamp =
+            chrono::DateTime::from_timestamp_millis(control_header.timestamp.timestamp_millis())
+                .unwrap();
+
+        assert_eq!(header, control_header);
+    }
+
+    #[test]
+    fn packet_to_bytes() {
+        let header = Header::new(&PAYLOAD);
+        let packet = Packet::new(header.clone(), PAYLOAD.to_vec()).unwrap();
+        let packet_bytes = packet.to_bytes();
+
+        let mut control_bytes = [0u8; HEADER_LEN + PAYLOAD.len()];
+        header.to_bytes(&mut control_bytes[..HEADER_LEN]);
+        control_bytes[HEADER_LEN..].copy_from_slice(&PAYLOAD);
+
+        assert_eq!(packet_bytes, control_bytes);
+    }
+
+    #[test]
+    fn packet_from_bytes() {}
+
+    #[test]
+    fn verify_checksum() {
+        let payload = [20, 69, 45, 60, 60, 80, 100];
+
+        let header = Header::new(&payload);
+
+        assert!(header.verify_checksum(&payload));
+    }
+
+    fn get_header() -> Header {
+        Header {
+            version: 1024,
+            timestamp: chrono::Utc::now(),
+            checksum: [20, 80, 1, 69],
+        }
+    }
+
+    fn get_header_control_bytes(header: &Header) -> [u8; HEADER_LEN] {
+        let mut control_bytes = [0u8; HEADER_LEN];
+        control_bytes[..4].copy_from_slice(&header.version().to_be_bytes());
+        control_bytes[4..12].copy_from_slice(&header.timestamp.timestamp_millis().to_be_bytes());
+        control_bytes[12..HEADER_LEN].copy_from_slice(&header.checksum);
+
+        control_bytes
     }
 }
