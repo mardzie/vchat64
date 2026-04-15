5f09a7013ca985988beb7c905f748ba03b4d5ffd
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2026-04-16 00:27:13 +0200

    Implemented FriendCode

--- src/app/friend_code.rs
+++ src/app/friend_code.rs
@@ -1,42 +1,32 @@
-use std::net::{IpAddr, SocketAddr};
+use std::{
+    net::{IpAddr, SocketAddr},
+    str::FromStr,
+};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct FriendCode(String);
 
 impl FriendCode {
     fn new(addr: SocketAddr) -> Self {
-        let mut addr_bytes: Vec<u8> = Vec::with_capacity(18); // 18 bytes to make space for ipv6 + port or ipv4 + port.
+        let mut addr_bytes: Vec<u8>;
         match addr.ip() {
-            std::net::IpAddr::V4(ipv4) => {
+            IpAddr::V4(ipv4) => {
+                addr_bytes = Vec::with_capacity(6);
                 addr_bytes.extend_from_slice(&ipv4.octets());
             }
-            std::net::IpAddr::V6(ipv6) => {
+            IpAddr::V6(ipv6) => {
+                addr_bytes = Vec::with_capacity(18);
                 addr_bytes.extend_from_slice(&ipv6.octets());
             }
         };
-
         addr_bytes.extend_from_slice(&addr.port().to_be_bytes());
 
         let hex = hex::encode(addr_bytes);
-        Self(
-            hex.chars()
-                .collect::<Vec<char>>()
-                .chunks(2)
-                .map(|chunk| chunk.iter().collect::<String>())
-                .collect::<Vec<String>>()
-                .join(" "),
-        )
+
+        Self(hex)
     }
 
-    pub fn from_ip_port(ip: IpAddr, port: u16) -> Self {
-        Self::new(SocketAddr::new(ip, port))
-    }
-    
-    pub fn from_string(addr: String) -> Result<Self, ()> {}
-    
-    pub fn from_string_ip(ip: String, port: u16) -> Result<Self, ()> {}
-
-    pub fn new_local(port: u16) -> Result<Self, String> {
+    pub fn new_local(port: u16) -> Result<Self, Error> {
         let ip = match local_ip_address::local_ip() {
             Ok(ip) => ip,
             Err(e) => {
@@ -48,7 +38,7 @@ impl FriendCode {
         Ok(Self::from_ip_port(ip, port))
     }
 
-    pub fn new_public(runtime: &tokio::runtime::Runtime, port: u16) -> Result<Self, String> {
+    pub fn new_public(runtime: &tokio::runtime::Runtime, port: u16) -> Result<Self, Error> {
         let ip = match runtime.block_on(public_ip_address::perform_lookup(None)) {
             Ok(lookup) => lookup.ip,
             Err(_) => {
@@ -60,10 +50,38 @@ impl FriendCode {
         Ok(Self::from_ip_port(ip, port))
     }
 
-    fn to_socket_addr(&self) -> Result<SocketAddr, ()> {
-        friend_code = friend_code.replace(" ", "");
+    pub fn from_ip_port(ip: IpAddr, port: u16) -> Self {
+        Self::new(SocketAddr::new(ip, port))
+    }
 
-        let bytes = match hex::decode(friend_code) {
+    pub fn from_string(addr: &str) -> Result<Self, Error> {
+        let addr = SocketAddr::from_str(addr).map_err(|_| ())?;
+
+        let mut addr_bytes: Vec<u8>;
+        match addr.ip() {
+            IpAddr::V4(ipv4) => {
+                addr_bytes = Vec::with_capacity(6);
+                addr_bytes.extend_from_slice(&ipv4.octets());
+            }
+            IpAddr::V6(ipv6) => {
+                addr_bytes = Vec::with_capacity(18);
+                addr_bytes.extend_from_slice(&ipv6.octets());
+            }
+        };
+        addr_bytes.extend_from_slice(&addr.port().to_be_bytes());
+
+        let friend_code = hex::encode(addr_bytes);
+        Ok(Self(friend_code))
+    }
+
+    pub fn from_string_ip(ip: &str, port: u16) -> Result<Self, Error> {
+        let mut addr = ip.to_string();
+        addr.push_str(&port.to_string());
+        Self::from_string(addr.as_str())
+    }
+
+    fn to_socket_addr(&self) -> Result<SocketAddr, Error> {
+        let bytes = match hex::decode(&self.0) {
             Ok(bytes) => bytes,
             Err(e) => {
                 log::warn!("Failed to decode friend code: {}", e);
@@ -108,10 +126,18 @@ impl From<SocketAddr> for FriendCode {
 }
 
 impl TryFrom<FriendCode> for SocketAddr {
-    type Error = ();
+    type Error = Error;
 
     fn try_from(friend_code: FriendCode) -> Result<Self, Self::Error> {
-        friend_code.to_socket_addr(friend_code)
+        friend_code.to_socket_addr()
+    }
+}
+
+impl TryFrom<&str> for FriendCode {
+    type Error = Error;
+
+    fn try_from(addr: &str) -> Result<Self, Self::Error> {
+        Self::from_string(addr)
     }
 }
 
@@ -120,3 +146,6 @@ impl std::fmt::Display for FriendCode {
         write!(f, "{}", self.as_str())
     }
 }
+
+#[derive(Debug, thiserror::Error, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Error {}
