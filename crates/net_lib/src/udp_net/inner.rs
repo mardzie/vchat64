f43e1fe652fc597d9ef4c5e32405b94b526f1e20
Marvin Dziedzina <marvin.dziedzina@gmail.com>
2026-05-27 14:49:19 +0200

    Added peek, recv and addresses functions

--- crates/net_lib/src/udp_net/inner.rs
+++ crates/net_lib/src/udp_net/inner.rs
@@ -1,6 +1,8 @@
 use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
 
-use crate::udp_net::error::{self, ConnectError, SendError};
+use crate::udp_net::error::{
+    self, ConnectError, LocalAddrError, PeekError, PeerAddrError, RecvError, SendError,
+};
 
 #[derive(Debug)]
 pub struct Inner {
@@ -18,10 +20,12 @@ impl Inner {
         Ok(self.socket.connect(addr)?)
     }
 
+    #[inline]
     pub fn send(&self, buf: &[u8]) -> Result<usize, SendError> {
         Ok(self.socket.send(buf)?)
     }
 
+    #[inline]
     pub fn send_to(&self, buf: &[u8], addr: impl ToSocketAddrs) -> Result<usize, SendError> {
         Ok(self.socket.send_to(buf, addr)?)
     }
@@ -45,12 +49,32 @@ impl Inner {
         }
     }
 
-    pub fn local_addr(&self) -> Result<SocketAddr, > {
-        self.socket.local_addr()
+    #[inline]
+    pub fn peek(&self, buf: &mut [u8]) -> Result<usize, PeekError> {
+        Ok(self.socket.peek(buf)?)
     }
 
-    pub fn peer_addr(&self) -> Result<SocketAddr, > {
-        self.socket.peer_addr()
+    #[inline]
+    pub fn peek_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr), PeekError> {
+        Ok(self.socket.peek_from(buf)?)
+    }
+
+    #[inline]
+    pub fn recv(&self, buf: &mut [u8]) -> Result<usize, RecvError> {
+        Ok(self.socket.recv(buf)?)
+    }
+
+    #[inline]
+    pub fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr), RecvError> {
+        Ok(self.socket.recv_from(buf)?)
+    }
+
+    pub fn local_addr(&self) -> Result<SocketAddr, LocalAddrError> {
+        Ok(self.socket.local_addr()?)
+    }
+
+    pub fn peer_addr(&self) -> Result<SocketAddr, PeerAddrError> {
+        Ok(self.socket.peer_addr()?)
     }
 
     pub fn try_clone(&self) -> Result<Self, error::BindError> {
