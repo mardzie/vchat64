3bac1aa62006cd066aab9970265c813dc2754046
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2026-05-30 01:52:57 +0200

    Added a max datagram size buffer preset

--- crates/net_lib/src/udp_net.rs
+++ crates/net_lib/src/udp_net.rs
@@ -33,13 +33,15 @@ const UDP_HEADER_LEN: usize = PORT_LEN + PORT_LEN + UDP_LENGTH_LEN + UDP_CHECKSU
 /// IPv4 header size is 20 bytes usually.
 /// IPv6 header size is 40 bytes.
 const IP_HEADER_LEN: usize = 40;
-/// The maximum buffer size.
+/// The common MTU (Maximum Transmission Unit) size of networks is 1500 bytes.
+const STANDARD_MTU: usize = 1500;
+/// The maximum buffer size the size of the biggest possible datagram.
+pub const MAX_SAFE_BUF_SIZE: usize = u16::MAX as usize;
+/// The safe maximum buffer size.
 /// It includes a IPv6 header of 40 bytes.
 ///
 /// `u16::MAX - UDP_HEADER_LEN (8 bytes) - IP_HEADER_LEN (40 bytes)`
-pub const MAX_BUF_SIZE: usize = u16::MAX as usize - UDP_HEADER_LEN - IP_HEADER_LEN;
-/// The common MTU (Maximum Transmission Unit) size of networks is 1500 bytes.
-const STANDARD_MTU: usize = 1500;
+pub const MAX_BUF_SIZE: usize = u16::MAX as usize - UDP_HEADER_LEN;
 /// The network safe size that fits in the common MTU (Maximum Transmission Unit) limit.
 ///
 /// This constant respects IPv4 normal headers and IPv6 headers.
