79241b938e491d0140d5a4b33724ef4c0843050f
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2026-05-30 14:02:08 +0200

    Added and fixed Inner tests

--- crates/net_lib/src/udp_net/inner.rs
+++ crates/net_lib/src/udp_net/inner.rs
@@ -171,10 +171,58 @@ mod tests {
         }
     }
 
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    enum BigPacket {
+        Option1(u32),
+        Option2(u32),
+    }
+
+    impl ToBytes for BigPacket {
+        fn to_bytes(&self, buf: &mut [u8]) -> Result<usize, InsufficientBuffer> {
+            if buf.len() <= 5 {
+                return Err(InsufficientBuffer);
+            }
+
+            let v = match self {
+                Self::Option1(v) => {
+                    buf[0] = 128;
+
+                    v
+                }
+                Self::Option2(v) => {
+                    buf[0] = 64;
+
+                    v
+                }
+            };
+
+            buf[1..5].copy_from_slice(&v.to_be_bytes());
+
+            Ok(5)
+        }
+    }
+
+    impl FromBytes for BigPacket {
+        fn from_bytes(buf: &[u8]) -> Result<Self, FromByteError> {
+            let mut v_bytes = [0u8; 4];
+            v_bytes.copy_from_slice(&buf[1..5]);
+            let v = u32::from_be_bytes(v_bytes);
+
+            match buf[0] {
+                128 => Ok(Self::Option1(v)),
+                64 => Ok(Self::Option2(v)),
+                _ => Err(FromByteError::InvalidData {
+                    offset: 0,
+                    desc: "Invalid enum".to_string(),
+                }),
+            }
+        }
+    }
+
     #[test]
     fn test_inner_connect() {
         let (inner1, addr1, inner2, addr2) = get_inners();
-        let mut buf: Box<[u8]> = Box::new([0u8; 1]);
+        let mut buf = [0u8; 2];
 
         inner1.connect(addr2).unwrap();
         inner2.connect(addr1).unwrap();
@@ -201,7 +249,7 @@ mod tests {
     #[test]
     fn test_inner_unconnected() {
         let (inner1, addr1, inner2, addr2) = get_inners();
-        let mut buf: Box<[u8]> = Box::new([0u8; 1]);
+        let mut buf = [0u8; 2];
 
         let first_packet = Packet::Option1;
         inner1.send_to(&first_packet, addr2, &mut buf).unwrap();
@@ -228,10 +276,46 @@ mod tests {
         assert_eq!(second_peeked_packet, second_recv_packet);
     }
 
-    fn get_inners() -> (Inner<Packet>, SocketAddr, Inner<Packet>, SocketAddr) {
-        let inner1: Inner<Packet> = Inner::bind("127.0.0.1:0").unwrap();
+    #[test]
+    fn test_inner_truncated() {
+        use core::assert_matches;
+
+        let (inner1, addr1, inner2, addr2) = get_inners();
+
+        inner1.connect(addr2).unwrap();
+        inner2.connect(addr1).unwrap();
+
+        let mut big_buf = [0u8; 256];
+        let first_packet = BigPacket::Option1(100);
+        inner1.send(&first_packet, &mut big_buf).unwrap(); // Sufficient large buffer to fit `BigPacket`.
+
+        let mut slightly_tiny_buf = [0u8; 5]; // Slightly too tiny buffer to fit a `BigPacket`.
+        let mut fitting_buf = [0u8; 6]; // Exactly fitting buf with +1 space to detect truncation.
+        assert_matches!(
+            inner2.peek(&mut slightly_tiny_buf),
+            Err(PeekError::DatagramTruncated)
+        );
+        let packet = inner2.recv(&mut fitting_buf).unwrap();
+        assert_eq!(packet, first_packet);
+
+        let second_packet = BigPacket::Option2(500);
+        inner2.send(&second_packet, &mut big_buf).unwrap();
+
+        let packet = inner1.peek(&mut fitting_buf).unwrap();
+        assert_eq!(packet, second_packet);
+        assert_matches!(
+            inner1.recv(&mut slightly_tiny_buf),
+            Err(RecvError::DatagramTruncated)
+        );
+    }
+
+    fn get_inners<P>() -> (Inner<P>, SocketAddr, Inner<P>, SocketAddr)
+    where
+        P: Bytes,
+    {
+        let inner1: Inner<P> = Inner::bind("127.0.0.1:0").unwrap();
         let addr1 = inner1.local_addr().unwrap();
-        let inner2: Inner<Packet> = Inner::bind("127.0.0.1:0").unwrap();
+        let inner2: Inner<P> = Inner::bind("127.0.0.1:0").unwrap();
         let addr2 = inner2.local_addr().unwrap();
 
         (inner1, addr1, inner2, addr2)
