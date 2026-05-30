fb1daf1c0ec6bcd4d753a85d478d203af03d53b2
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2026-05-30 14:27:11 +0200

    Fixed correctness of check_for_truncation()

--- crates/net_lib/src/udp_net/inner.rs
+++ crates/net_lib/src/udp_net/inner.rs
@@ -119,7 +119,7 @@ where
     /// Uses the last byte as an indicator that the datagram was truncated.
     /// This does not hold true when the buffer is the max datagram size.
     fn check_for_truncation(buf: &[u8], len: usize) -> Result<(), RecvError> {
-        if buf.len() < MAX_DATAGRAM_SIZE && len >= buf.len() {
+        if buf.len() < MAX_DATAGRAM_SIZE && len == buf.len() {
             return Err(PeekError::DatagramTruncated);
         }
 
