e19e2d5adb311c9ffb3baf13212774b328aef402
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2026-09-21 23:42:54 +0200

    Added Stream test

---
+++ crates/opus_cpal_bridge/tests/voice_round_trip_test.rs
@@ -0,0 +1,22 @@
+use opus_cpal_bridge::stream::{
+    Stream,
+    stream_inner::{Input, Output},
+};
+use ringbuf::traits::{Consumer, Producer};
+
+const RINGBUF_SIZE: usize = 4 * 1024;
+
+#[test]
+fn voice_round_trip() {
+    let host = cpal::default_host();
+    let (input, mut consumer) = Stream::<Input>::new(&host, RINGBUF_SIZE).unwrap();
+    let (output, mut producer) = Stream::<Output>::new(&host, RINGBUF_SIZE).unwrap();
+
+    tracing::info!("Input Config: {:?}", input.config());
+
+    loop {
+        let count = producer.push_iter(consumer.pop_iter());
+        tracing::info!("Moved {} samples from Input to Output.", count);
+        std::thread::park_timeout(std::time::Duration::from_millis(5));
+    }
+}
