b7c7319ec73ec20b0c96a450290b39e69f59d32a
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2026-09-21 16:19:50 +0200

    Added Debug impl

--- crates/opus_cpal_bridge/src/stream.rs
+++ crates/opus_cpal_bridge/src/stream.rs
@@ -94,6 +94,14 @@ impl<D: Direction + BufferDirection> Deref for Stream<D> {
     }
 }
 
+impl<D: Direction> Debug for Stream<D> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Stream")
+            .field("inner", &self.inner)
+            .finish_non_exhaustive()
+    }
+}
+
 #[must_use]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum DeviceType {
